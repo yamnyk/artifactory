eafddf6f80c3a37377fa5b937f342b88eb922408
yamnyk <yamnyk.vlog@gmail.com>
2025-04-06 20:32:02 -0400

    maybe removed 404 on deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/artifactory/',
   plugins: [preact()],
   resolve: {
     alias: {
