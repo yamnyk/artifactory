b4b30d7f99186e1de6963e6a3c6aa5b98f4f4d15
yamnyk <yamnyk.vlog@gmail.com>
2025-04-19 12:28:37 -0400

    typo

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,24 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const isGh = mode === 'gh';
-  return ({
-  base: isGh ? '/artifactory/' : '/',
-  plugins: [preact()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        fallback: 'index.html',
+  return {
+    base: isGh ? '/artifactory/' : '/',
+    plugins: [preact()],
+    resolve: {
+      alias: {
+        '@': '/src',
       },
     },
-  },
-}));
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+          fallback: 'index.html',
+        },
+      },
+    },
+  };
+});
