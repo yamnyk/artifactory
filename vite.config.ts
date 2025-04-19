91aff9587f69012f008ecd09b5b2bb58f54d8bf6
yamnyk <yamnyk.vlog@gmail.com>
2025-04-19 13:03:03 -0400

    htaccess shennanigans

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const isGh = mode === 'gh';
   return {
     base: isGh ? '/artifactory/' : '/',
-    plugins: [preact()],
+    plugins: [
+      preact(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'public/_htaccess',
+            dest: '.', // root of dist/
+            rename: '.htaccess',
+          },
+        ],
+      }),
+    ],
     resolve: {
       alias: {
         '@': '/src',
